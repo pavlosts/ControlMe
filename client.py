afe1c409a1e450452d425fe99829839d5a22de0b
pavlosts <pavlos_tsaou@hotmail.gr>
2014-11-24 20:22:48 +0200

    Get hostname and connect to server
    Added the code to get server's hostname and then connect to server. Also
    added code for error handling.

--- client.py
+++ client.py
@@ -1,4 +1,3 @@
-
 import socket  # Necessary for sockets
 import sys  # Necessary for "exit"
 
@@ -7,6 +6,25 @@ try:
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 except socket.error:
     print('Failed to create socket...')
+    sys.exit()
 
 print('Socket created!')
+
+host = 'localhost'
+port = 80
+
+try:
+    server = socket.gethostbyname(host)
+except socket.gaierror:
+    print('Error resolving hostname...')
+    sys.exit()
+try:
+    # Connects to remote server
+    res = sock.connect_ex((server, port))
+    print(res)
+except res is not 0:
+    print('Error connectin to server')
+
+print('You are now connected to server: ', host, ' with IP ', server)
+
 
