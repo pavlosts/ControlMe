21302c691ba1761f1bfd30ad09c6b3057f97279f
pavlosts <pavlos_tsaou@hotmail.gr>
2014-11-24 16:13:33 +0200

    Socket Creation
    Added the code to create the socket

--- client.py
+++ client.py
@@ -1,1 +1,12 @@
 
+import socket  # Necessary for sockets
+import sys  # Necessary for "exit"
+
+try:
+    # Creates a socket
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+except socket.error:
+    print('Failed to create socket...')
+
+print('Socket created!')
+
