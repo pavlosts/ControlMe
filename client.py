af660b5b57966170be19c618366bc7d3e7173de8
pavlosts <pavlos_tsaou@hotmail.gr>
2014-11-25 18:28:33 +0200

    Communicate and close
    The client can now send and receive message from the server. It has also
    been added the command to close the socket.

--- client.py
+++ client.py
@@ -18,13 +18,32 @@ try:
 except socket.gaierror:
     print('Error resolving hostname...')
     sys.exit()
+
 try:
     # Connects to remote server
-    res = sock.connect_ex((server, port))
-    print(res)
+     res = sock.connect_ex((server, port))
 except res is not 0:
-    print('Error connectin to server')
+    print('Error connection to server')
+    sys.exit()
 
 print('You are now connected to server: ', host, ' with IP ', server)
+
+# Inform server that client is connected
+msg = b"Client is now connected."
+
+try:
+    sock.sendall(msg)
+except socket.error:
+    print('Failed to send message to server')
+    sys.exit()
+
+print('Server knows client is connected')
+
+# Recieve data from server
+reply = sock.recv(4096)
+
+print(reply)
+
+sock.close()
 
 
