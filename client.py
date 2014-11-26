73ccf009b2c6173a506a2e83e788cd4e9617a4c4
pavlosts <pavlos_tsaou@hotmail.gr>
2014-11-26 10:35:35 +0200

    Minor changes
    It now prints both hostname and IP of the server. It also waits for user
    to press ENTER in order to close the program.

--- client.py
+++ client.py
@@ -11,23 +11,23 @@ except socket.error:
 print('Socket created!')
 
 print('Enter host\'s IP')
-server = input()
+ip = input()
 port = 80
 
 try:
-    server = socket.gethostbyname(server)
+    server = socket.gethostbyname(ip)
 except socket.gaierror:
     print('Error resolving hostname...')
     sys.exit()
 
 try:
     # Connects to remote server
-     res = sock.connect_ex((server, port))
+    res = sock.connect_ex((server, port))
 except res is not 0:
     print('Error connection to server')
     sys.exit()
 
-print('You are now connected to server: ', server, ' with IP ', server)
+print('You are now connected to server: ', socket.getfqdn(ip), ' with IP ', ip)
 
 # Inform server that client is connected
 msg = b"Client is now connected."
@@ -40,11 +40,12 @@ except socket.error:
 
 print('Server knows client is connected')
 
-# Recieve data from server
+# Receive data from server
 reply = sock.recv(4096)
 
 print(reply)
 
 sock.close()
 
-
+print("Press enter to close program")
+input()
